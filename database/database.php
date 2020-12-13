6806ba7ef0ba0fa14c28cb8c20989b6dbb9402ea
Doğu <dogu_nigar_fb@hotmail.com>
2020-12-13 17:53:19 +0300

    Revert "r"
    This reverts commit 18b435df88fa2797a703d347bc39bd2511b03665.

--- database/database.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $dbconnect = mysqli_connect("localhost", "root", "") or die ("Could not connect to the server.");
-    mysqli_select_db($dbconnect, "ingweb") or die ("The database could not be selected.");
-    mysqli_set_charset($dbconnect, 'utf8');
-?>
